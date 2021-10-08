de36df8f64b954cd05971cc181396e4d30409d33
Mark Kozlov <mark.k.2012@yandex.ru>
2021-10-08 09:05:24 +0300

    Обновил детерминант у себя1

--- determinant.py
+++ determinant.py
@@ -8,12 +8,10 @@ def get_matrix_minor(mat, column, row=0):
     :param column: столбец, от которого нужно избавиться, индексация с нуля
     :return: минор матрицы
     """
-    print(mat, "**")
     mat1 = deepcopy(mat)
     mat1.pop(row)
     for el in range(0, len(mat1)):
         mat1[el].pop(column)
-    print(mat1, "!!")
     return mat1
 
 
@@ -23,18 +21,14 @@ def compute_det(matrix) -> int:
     :return: определитель матрицы
     """
     if len(matrix) == 2:
-        print("exit")
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
     elif len(matrix) == 3:
-        print('exit')
         return matrix[0][0] * matrix[1][1] * matrix [2][2] + matrix[0][1] * matrix[1][2] * matrix[2][0] + matrix[0][2] * matrix[1][0] * matrix[2][1] - matrix[0][2] * matrix[1][1] * matrix[2][0] - matrix[0][0] * matrix[1][2] * matrix[2][1] - matrix[0][1] * matrix[1][0] * matrix[2][2]
     else:
-        # return sum([((-1)**j * matrix[0][j] * compute_det(get_matrix_minor(matrix, j))) for j in range(0, len(matrix))])
-        print(matrix)
         count = 0
         plan = []
-        count_0 = [0,sum(matrix[0]),0]  # Счётчик оптимизации с наибольшим чилом нулей и наименьшем модулем коэфициентов (Только для строки!)
-        k = 0   #  Счётчик строки
+        count_0 = [0, sum(matrix[0]), 0]  # Счётчик оптимизации с наибольшим чилом нулей и наименьшем модулем коэфициентов (Только для строки!)
+        k = 0   # Счётчик строки
         for stroka in matrix:
             if count_0[0] < stroka.count(0):
                 count_0[0] = stroka.count(0)
@@ -42,12 +36,11 @@ def compute_det(matrix) -> int:
             elif abs(sum(stroka)) <= count_0[1] and count_0[0] == stroka.count(0):
                     count_0[1] = abs(sum(stroka))
                     count_0[2] = k
-            k +=1
+            k += 1
 
         for item in matrix[count_0[2]]:
             plan.append((-1) ** count * item * compute_det(get_matrix_minor(matrix, count)))
             count += 1
-            print(plan, count, len(matrix))
         return sum(plan)
 
 
