5b2fafb38b429b8a0cb75b2e7590941c8144bce0
Archi20Rad <202103@edu.fa.ru>
2021-10-08 10:24:03 +0300

    Update determinant.py

--- determinant.py
+++ determinant.py
@@ -61,3 +61,36 @@ def numpy_linalg_det(matrix):
     return numpy.linalg.det(matrix)
 
 numpy_linalg_det([[10, 2, 5, 60], [7, 9, 0, 78], [15, 22, 65, 111], [50, 100, 17, 33]])
+
+
+# Сравнение скорости работы нашего нахождения детерминанта с numpy
+import time
+from matrix_generator import matrix_generator
+import matplotlib.pyplot as plt
+time_our = []
+time_np = []
+size_m = [] # 1*1 , 2*2, 3*3, 4*4, 5*5,  и т.д.
+
+for i in range(1, 8):
+    m = matrix_generator(i,i)
+    time_start  = time.time()
+    numpy_linalg_det(m)  # Нахождение детерминанта numpy
+    elapsed_time = time.time() - time_start
+    time_np.append(elapsed_time)
+
+    time_start  = time.time()
+    compute_det(m)   # Наше нахождение детерминанта 
+    elapsed_time = time.time() - time_start
+    time_our.append(elapsed_time)
+    
+    size_m.append(i)
+
+fig, ax = plt.subplots()
+ax.plot(size_m, time_our, 'co-',label = 'Наше нахождение детерминанта')
+ax.plot(size_m, time_np, 'r--',label = 'Numpy')
+ax.set(xlabel = 'Размер матрицы x*x', ylabel = "Время в секундах")
+ax.legend(loc = 'upper left')
+plt.title('Скорость выполнения функции',loc = 'center', pad = 10 )
+fig.set_figwidth(12)
+fig.set_figheight(6)
+plt.show()
