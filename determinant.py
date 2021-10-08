f870081bdd0c081546a9aa0dac287e1ef21bf132
Archi20Rad <202103@edu.fa.ru>
2021-10-08 10:07:57 +0300

    Update determinant.py

--- determinant.py
+++ determinant.py
@@ -22,10 +22,11 @@ def compute_det(matrix) -> int:
     :param matrix: квадратная матрица
     :return: определитель матрицы
     """
-    if len(matrix) == 2:
+  
+    if len(matrix) == 2 and len(matrix[0]) == 2:
         print("exit")
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-    elif len(matrix) == 3:
+    elif len(matrix) == 3 and len(matrix[0]) == 3:
         print('exit')
         return matrix[0][0] * matrix[1][1] * matrix [2][2] + matrix[0][1] * matrix[1][2] * matrix[2][0] + matrix[0][2] * matrix[1][0] * matrix[2][1] - matrix[0][2] * matrix[1][1] * matrix[2][0] - matrix[0][0] * matrix[1][2] * matrix[2][1] - matrix[0][1] * matrix[1][0] * matrix[2][2] 
     else:
@@ -54,3 +55,9 @@ def compute_det(matrix) -> int:
 if __name__ == '__main__':
     print(compute_det(matrix=[[10, 2, 5, 60], [7, 9, 0, 78], [15, 22, 65, 111], [50, 100, 17, 33]]))
     # print(get_matrix_minor(mat=[[1, 2, 3], [4, 5, 6], [7, 8, 9]], column=0))
+
+def numpy_linalg_det(matrix):
+    import numpy
+    return numpy.linalg.det(matrix)
+
+numpy_linalg_det([[10, 2, 5, 60], [7, 9, 0, 78], [15, 22, 65, 111], [50, 100, 17, 33]])
