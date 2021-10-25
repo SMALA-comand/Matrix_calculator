e57c0c732707db5f790c7ff7048bc988ca54ae6b
Archi20Rad <202103@edu.fa.ru>
2021-10-25 21:02:41 +0300

    Наконец-то рабочий Якоби!

--- Jacobi_SofALE.py
+++ Jacobi_SofALE.py
@@ -2,7 +2,7 @@
 from Gauss_SofALE import solve_gauss
 import numpy as np
 
-def solve_jacobi(m, acc=10**(-6)):
+def solve_jacobi(m, acc=10**(-7)):
     """
     :param m: Исходная матрица
     :param acc: Точность вычислений у метода Якоби
@@ -14,16 +14,26 @@ def solve_jacobi(m, acc=10**(-6)):
     count = 0     # Кол-во интераций (если больше тысячи, то переходим к Гауссу)
     x_new = [1.0 for i in range(len(m[0]))]
     x_prev = [0.0 for i in range(len(m[0]))]
-    delta = max(abs(max(x_prev) - min(x_new )),abs(max(x_new ) - min(x_prev)))
-    delta_prev = delta
-    while delta > acc:
-        delta_prev = delta
+    delta = 1
+
+    # Проверка диагонального преобладания
+    for i in range(len(m)):
+        summa = 0
+        for j in range(len(m[i])):
+            if i != j:
+                summa += abs(m[i][j])
+        if abs(m[i][i]) <= summa:
+            print('Не соблюдается диагональное преобладание')
+            print('Пробуем решить Якоби: ...')
+            break
+
+    while count < 1001:
+        if count == 1000:
+            print('Больше тысячи интераций, переходим к Гауссу')
+            return solve_gauss(matrix)
         x_new = []
         count += 1
-        if count == 1001:
-            #print('Больше тысячи интераций, переходим к Гауссу')
-            #solve_gauss(matrix)
-            return x_prev
+
         for i in range(len(m)):
             dif = 0
             k = 0
@@ -32,10 +42,26 @@ def solve_jacobi(m, acc=10**(-6)):
                     dif -= el*x_prev[k]
                 k += 1
             x_new.append((x_right[i]+dif)/m[i][i])
+
+        flag = is_need_to_complete(x_prev, x_new, acc)
+        if flag:
+            break
         x_prev = x_new
-        delta = max(abs(max(x_prev) - min(x_new)), abs(max(x_new) - min(x_prev)))
-        difference = delta_prev - delta
+
     return x_prev
+
+def is_need_to_complete(x_prev, x_new, acc):
+    """
+        :param x_prev: Предыдущие решение СЛАУ
+        :param x_new: Новое решение СЛАУ (последнее)
+        :param acc: Точность вычислений у метода Якоби
+    """
+    sum_up = 0
+    sum_low = 0
+    for k in range(len(x_prev)):
+        sum_up += (x_new[k] - x_prev[k]) ** 2
+        sum_low += (x_new[k]) ** 2
+    return ((sum_up / sum_low)**0.5) < acc
 
 if __name__ == '__main__':
     print(solve_jacobi(m = [[2.6,-1.7,2.5,3.7],[1.5,6.2,-2.9,3.2],[2.8,-1.7,3.8,2.8]]))
