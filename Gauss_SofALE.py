8b66319f26d81388c78d66e9eee48b60d119e9ab
Archi20Rad <202103@edu.fa.ru>
2021-10-25 17:26:33 +0300

    + Обратная матрица через Гаусса

--- Gauss_SofALE.py
+++ Gauss_SofALE.py
@@ -93,7 +93,46 @@ def solve_gauss_fractions(m):
 
     return x
 
+
+def inverse_matrix(matrix_origin):
+    """
+    :param matrix_origin: Исходная матрица
+    """
+    matrix_origin = np.array(matrix_origin)
+    m = np.hstack((matrix_origin,
+                   np.matrix(np.diag([1.0 for i in range(matrix_origin.shape[0])]))))
+    # Прямой ход
+    for k in range(len(m)):
+        swap_row = pick_nonzero_row(m, k)  # Меняем местами k-строку с одной из нижележащих, если m[k, k] = 0
+        if swap_row != k:
+            m[k, :], m[swap_row, :] = m[swap_row, :], np.copy(m[k, :])
+        # Делаем диагональный элемент равным 1
+        if m[k, k] != 1:
+            m[k, :] *= 1 / m[k, k]
+        for row in range(k + 1, len(m)):
+            m[row, :] -= m[k, :] * m[row, k]
+
+    # Обратный ход
+    for k in range(len(m) - 1, 0, -1):
+        for row in range(k - 1, -1, -1):
+            if m[row, k]:
+                #  Делаем все вышележащие элементы равными нулю в прежней матрице идентичности
+                m[row, :] -= m[k, :] * m[row, k]
+
+    return np.hsplit(m, len(m) // 2)[1]
+
+
+def pick_nonzero_row(m, k):
+    """
+    :param m: Исходная матрица
+    :param k: k-строка матрицы
+    """
+
+    while k < m.shape[0] and not m[k, k]:
+        k += 1
+    return k
+
 if __name__ == '__main__':
-    #print(solve_gauss_fractions(m = [[2.6,-1.7,2.5,3.7],[1.5,6.2,-2.9,3.2],[2.8,-1.7,3.8,2.8]]))
+    print(solve_gauss_fractions(m = [[2.6,-1.7,2.5,3.7],[1.5,6.2,-2.9,3.2],[2.8,-1.7,3.8,2.8]]))
     #print(solve_gauss_fractions(m = [[1,2,3,6],[2,3,1,6],[3,1,2,6]]))
-    print(solve_gauss_fractions(m=[[0, 0, 0, 1], [0, 0, 0,0], [0, 0, 0, 0]]))
+    #print(solve_gauss_fractions(m=[[0, 0, 0, 1], [0, 0, 0,0], [0, 0, 0, 0]]))
