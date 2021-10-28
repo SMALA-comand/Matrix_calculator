09e01e2a1e0f7c9c6d52038ad8a1e1b595ed906f
Mark Kozlov <mark.k.2012@yandex.ru>
2021-10-28 19:22:54 +0300

    Сделал функцию определителя

--- main_qt.py
+++ main_qt.py
@@ -2,34 +2,31 @@ import math
 import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import *
+from determinant import compute_det
 
 
 class App(QMainWindow):
     d = []
+
     def __init__(self):
         QMainWindow.__init__(self)
         self.plane = uic.loadUi('calc_ui.ui')
-        self.otr()
         self.determinant()
         self.plane.show()
 
-    def otr(self):
-        for i in range(0, 81):
+    def otrisovka(self, n):
+        ds = []
+        input_text = n
+        one_side = int(math.sqrt(input_text))
+        for i in range(0, n):
             line = QLineEdit(f'input{i}', self.plane.tab3)
             line.resize(70, 30)
             line.setText('')
             line.setPlaceholderText('5')
-            line.move(20 + (i % 9) * 70, 100 + 30 * (i // 9))
-            line.hide()
-            self.d.append(line)
-
-    def otrisovka(self, n):
-        input_text = n
-        one_side = int(math.sqrt(input_text))
-        for i in range(0, n):
-            self.d[i].show()
-        for i in range(n, 81):
-            self.d[i].hide()
+            line.move(20 + (i % one_side) * 70, 100 + 30 * (i // one_side))
+            line.show()
+            ds.append(line)
+        self.d = ds
 
     def true_input(self):
         if self.plane.lineEdit.displayText() == '':
@@ -37,8 +34,19 @@ class App(QMainWindow):
         else:
             self.otrisovka(int(self.plane.lineEdit.displayText()))
 
+    def compute_determ(self):
+        matrix = []
+        a = int(len(self.d)**0.5)
+        for i in range(a):
+            matrix.append([])
+            lines = self.d[i*a:i*a+a]
+            for j in range(a):
+                matrix[i].append(int(lines[j].displayText()))
+        print(compute_det(matrix=matrix))
+
     def determinant(self):
         self.plane.pushButton.clicked.connect(self.true_input)
+        self.plane.pushButton_2.clicked.connect(self.compute_determ)
 
 
 if __name__ == '__main__':
