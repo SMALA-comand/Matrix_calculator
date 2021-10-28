3ed06825dc93e9a19f6add1204a9936fbb08ae32
Mark Kozlov <mark.k.2012@yandex.ru>
2021-10-28 18:32:33 +0300

    Что-то наклевывается

--- main_qt.py
+++ main_qt.py
@@ -5,40 +5,40 @@ from PyQt5.QtWidgets import *
 
 
 class App(QMainWindow):
+    d = []
     def __init__(self):
         QMainWindow.__init__(self)
         self.plane = uic.loadUi('calc_ui.ui')
-        self.matrix_expression()
-        self.transposing()
+        self.otr()
         self.determinant()
-        self.obuslov()
-        self.slau()
         self.plane.show()
 
-    def otrisovka(self):
-        pass
-
-    def matrix_expression(self):
-        pass
-
-    def transposing(self):
-        pass
-
-    def determinant(self):
-        input_text = 81
-        one_side = int(math.sqrt(input_text))
-        for i in range(0, input_text):
+    def otr(self):
+        for i in range(0, 81):
             line = QLineEdit(f'input{i}', self.plane.tab3)
             line.resize(70, 30)
             line.setText('')
             line.setPlaceholderText('5')
-            line.move(20 + (i % one_side) * 70, 100 + 30 * (i // one_side))
+            line.move(20 + (i % 9) * 70, 100 + 30 * (i // 9))
+            line.hide()
+            self.d.append(line)
 
-    def obuslov(self):
-        pass
+    def otrisovka(self, n):
+        input_text = n
+        one_side = int(math.sqrt(input_text))
+        for i in range(0, n):
+            self.d[i].show()
+        for i in range(n, 81):
+            self.d[i].hide()
 
-    def slau(self):
-        pass
+    def true_input(self):
+        if self.plane.lineEdit.displayText() == '':
+            print('bbc')
+        else:
+            self.otrisovka(int(self.plane.lineEdit.displayText()))
+
+    def determinant(self):
+        self.plane.pushButton.clicked.connect(self.true_input)
 
 
 if __name__ == '__main__':
